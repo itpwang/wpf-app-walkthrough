519483a568a91ee8228314ae6a16f0911d0edd5f
itpwang <ivan.wang.1995@gmail.com>
2017-07-19 14:01:45 -0700

    Added layout and controls.

--- WpfApplicationWalkthrough/MainWindow.xaml.cs
+++ WpfApplicationWalkthrough/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace WpfApplicationWalkthrough
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
